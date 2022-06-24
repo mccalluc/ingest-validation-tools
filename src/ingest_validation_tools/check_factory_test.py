6dc44a1162219ab5a4fbf527f5d5eb494cb2e635
mccalluc <mccalluc@users.noreply.github.com>
2022-06-24 16:46:50 -0400

    test custom constraint

--- src/ingest_validation_tools/check_factory_test.py
+++ src/ingest_validation_tools/check_factory_test.py
@@ -18,7 +18,17 @@ def validate(schema):
     return tasks[0]['errors']
 
 
-def test_expected_error():
+def test_good_data():
+    schema = {
+        'fields': [
+            {'name': 'a_field'}
+        ]
+    }
+    errors = validate(schema)
+    assert not errors
+
+
+def test_bad_column_name():
     schema = {
         'fields': [
             {'name': 'this_name_will_not_match'}
@@ -27,3 +37,19 @@ def test_expected_error():
     errors = validate(schema)
     assert errors[0]['description'] == \
         'One of the data source header does not match the field name defined in the schema.'
+
+
+def test_custom_constraint():
+    schema = {
+        'fields': [
+            {
+                'name': 'a_field',
+                'custom_constraints': {
+                    'forbid_na': True
+                }
+            }
+        ]
+    }
+    errors = validate(schema)
+    assert errors[0]['note'] == \
+        '"N/A" fields should just be left empty'
