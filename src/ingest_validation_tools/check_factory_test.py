8a01ee66db0e47dc073cb34e7398533d1971e562
mccalluc <mccalluc@users.noreply.github.com>
2022-06-24 16:30:33 -0400

    add CSV fixture

--- src/ingest_validation_tools/check_factory_test.py
+++ src/ingest_validation_tools/check_factory_test.py
@@ -1,11 +1,29 @@
+from pathlib import Path
+
+import frictionless
+
 from .check_factory import make_checks
 
-def test_fake():
-    make_checks(schema={
+
+def validate(schema):
+    checks = make_checks(schema=schema)
+    report = frictionless.validate(
+        source=(Path(__file__).parent / 'check_factory_test_fixture.csv').absolute(),
+        schema=schema,
+        checks=checks
+    )
+    assert not report['errors']
+    return report
+
+
+def test_expected_error():
+    schema = {
         'fields': [
-            {
-                'name': 'a_field',
-                'custom_constraints': []
-            }
+            {'name': 'this_name_will_not_match'}
         ]
-    })
+    }
+    report = validate(schema)
+    tasks = report['tasks']
+    assert len(tasks) == 1
+    assert tasks[0]['errors'][0]['description'] == \
+        'One of the data source header does not match the field name defined in the schema.'
